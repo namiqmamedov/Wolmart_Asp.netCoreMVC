a3b5c47abd5960d292a3a522a823b3602a0c6656
namiqmamedov <namigmm@code.edu.az>
2023-05-21 23:00:15 +0400

    Updated controllers

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wolmart.Ecommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int? id)
+        {
+            return View();  
+        }
+    }
+}
