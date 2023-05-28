8ef4aeace163d23143dd71992959ee46f8a53f7e
namiqmamedov <namigmm@code.edu.az>
2023-05-28 20:59:03 +0400

    Updated admin area

---
+++ Areas/admin/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Wolmart.Ecommerce.DAL;
+using Wolmart.Ecommerce.Models;
+
+namespace Wolmart.Ecommerce.Areas.admin.Controllers
+{
+    [Area("admin")]
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products.ToListAsync());
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if(!ModelState.IsValid) { return View();}
+
+            return View();
+        }
+
+
+    }
+}
